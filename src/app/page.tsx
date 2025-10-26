4419d923f8afc7ab338a3a1f0599f643e02fccff
GOD_OF_ENTITIES <herobrineking2010@gmail.com>
2025-10-26 09:21:58 +0000

    if i pressed to hide side bar then it must have a button to get the side

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,10 @@ import {
   SidebarProvider,
   Sidebar,
   SidebarInset,
+  SidebarTrigger,
 } from '@/components/ui/sidebar';
+import { Button } from '@/components/ui/button';
+import { PanelLeft } from 'lucide-react';
 
 export default function Home() {
   return (
@@ -12,7 +15,10 @@ export default function Home() {
       <div className="flex min-h-screen bg-background text-foreground">
         <HistorySidebar />
         <SidebarInset>
-          <main className="flex flex-1 flex-col items-center justify-center p-4">
+          <main className="relative flex flex-1 flex-col items-center justify-center p-4">
+            <div className="absolute top-4 left-4">
+               <SidebarTrigger className="md:hidden" />
+            </div>
             <ChatLayout />
           </main>
         </SidebarInset>
