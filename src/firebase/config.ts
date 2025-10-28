511356b819f06688dda80589af480c64e1f41020
GOD_OF_ENTITIES <herobrineking2010@gmail.com>
2025-10-28 07:16:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2861372455-f02d7",
+  "appId": "1:835341565361:web:56e3ab255ea8c62465d190",
+  "apiKey": "AIzaSyCsNArGAzihFJIjLsYO_09y1Q5eEZWDR2I",
+  "authDomain": "studio-2861372455-f02d7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "835341565361"
+};
